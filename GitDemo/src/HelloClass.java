9c9f24e00cec4a9615f019f90fd923ad7e86a933
Veena Saini <veena.sainiaug2shine@gmail.com>
2018-02-21 10:35:58 +0530

    Hello Rajat

--- GitDemo/src/HelloClass.java
+++ GitDemo/src/HelloClass.java
@@ -4,7 +4,7 @@ public class HelloClass {
 	public static void main(String args[])
 	{
 		for(int i=1;i<=10;i++)
-			System.out.println("Hello Apexit "+i);
+			System.out.println("Hello Rajat "+i);
 	}
 	
 }
